877cfc7ec02d5be50469e5934c8a86d6c227256d
Fenton Radford <fentirad>
2017-04-24 21:01:29 -0700

    GameController
    spawning a single hazard

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValue;
+
+	void Start () {
+		SpawnWaves ();
+	}
+
+	void SpawnWaves() {
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+		Quaternion spawnRotation = Quaternion.identity;
+
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+}
